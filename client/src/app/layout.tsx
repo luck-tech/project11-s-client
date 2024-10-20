a0420db9149b696ea6f5956a1f090410d55bf6e9
Imai Raku <71510837+luck-tech@users.noreply.github.com>
2024-10-20 14:11:15 +0900

    Revert "モバイル参加画面"

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -13,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body suppressHydrationWarning className="font-['Note_Sans_JP']">
-        {children}
-      </body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
